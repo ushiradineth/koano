85ac7a1b3f62c3636dc087a51b92a14281485fa8
Ushira Dineth <ushiradineth@gmail.com>
2024-02-11 08:43:12 +0530

    chore: init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				text: "var(--text)",
+				background: "var(--background)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				accent: "var(--accent)",
+				muted: "var(--muted)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
