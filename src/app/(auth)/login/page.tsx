714f9d693e89d058d7b698606478505d9240f89f
Ushira Dineth <ushiradineth@gmail.com>
2024-02-18 22:56:20 +0530

    feat: redirection

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,8 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+	redirect("/dashboard");
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between bg-background p-24">
 			<div className="w-full max-w-5xl items-center justify-between text-sm lg:flex">
