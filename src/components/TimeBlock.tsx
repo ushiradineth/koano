c0679af6ef27cddfb86ebd7a07aab1bedf4d0f65
Ushira Dineth <ushiradineth@gmail.com>
2024-02-18 21:03:46 +0530

    fix: remove time when current time is displayed

--- src/components/TimeBlock.tsx
+++ src/components/TimeBlock.tsx
@@ -8,24 +8,22 @@ export default function TimeBlock({ hour }: Props) {
 	const currentHour = getCurrentHourTime(hour - 1);
 
 	return (
-		<>
-			<div className="flex h-[60px] relative w-full items-center justify-end px-2 font-mono font-medium">
+		<div className="flex h-[60px] relative w-full items-center justify-center px-2 font-mono font-medium">
+			<p
+				className={cn(
+					"absolute top-12",
+					currentHour.isPreviousHour && currentHour.minutes < 20 && "hidden",
+					currentHour.isCurrentHour && currentHour.minutes > 40 && "hidden",
+				)}>
+				{getHour(hour)}
+			</p>
+			{currentHour.isCurrentHour && (
 				<p
-					className={cn(
-						"absolute top-12",
-						currentHour.isPreviousHour && currentHour.minutes < 10 && "hidden",
-						currentHour.isCurrentHour && currentHour.minutes > 50 && "hidden",
-					)}>
-					{getHour(hour)}
+					className={cn("absolute bg-background")}
+					style={{ top: currentHour.minutes - 12 }}>
+					{currentHour.time}
 				</p>
-				{currentHour.isCurrentHour && (
-					<p
-						className={cn("absolute bg-background")}
-						style={{ top: currentHour.minutes - 6 }}>
-						{currentHour.time}
-					</p>
-				)}
-			</div>
-		</>
+			)}
+		</div>
 	);
 }
