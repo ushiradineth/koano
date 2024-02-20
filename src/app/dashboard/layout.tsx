22983200c7086ccfa452d64a0d9d3ae38bdd7e86
Ushira Dineth <ushiradineth@gmail.com>
2024-02-20 09:30:51 +0530

    feat: added useContext for local event and data persistance

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,61 @@
-import { Suspense } from "react";
+"use client";
+
+import { repeatValues } from "@/lib/consts";
+import { Event, PickerType } from "@/lib/types";
+import { generateTimeArray, generateTimezoneArray } from "@/lib/utils";
+import {
+	Suspense,
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
+
+type EventContextType = {
+	events: Event[];
+	setEvents: (value: Event[]) => void;
+};
+
+const EventContext = createContext<EventContextType>({
+	events: [],
+	setEvents: () => undefined,
+});
+
+export const useEventContext = () => useContext(EventContext);
+
+type DataContextType = {
+	time: PickerType[];
+	timezone: PickerType[];
+	repeated: PickerType[];
+};
+
+const DataContext = createContext<DataContextType>({
+	time: [],
+	timezone: [],
+	repeated: [],
+});
+
+export const useDataContext = () => useContext(DataContext);
 
 export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const [events, setEvents] = useState<Event[]>([]);
+
 	return (
 		<section>
-			<Suspense>{children}</Suspense>
+			<EventContext.Provider value={{ events, setEvents }}>
+				<DataContext.Provider
+					value={{
+						time: generateTimeArray(),
+						timezone: generateTimezoneArray(),
+						repeated: repeatValues,
+					}}>
+					<Suspense>{children}</Suspense>
+				</DataContext.Provider>
+			</EventContext.Provider>
 		</section>
 	);
 }
