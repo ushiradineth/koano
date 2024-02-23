bece51e1faf2a82732fd4b164f5179b328bfd10e
Ushira Dineth <ushiradineth@gmail.com>
2024-02-23 23:00:04 +0530

    chore: extracted logo to component

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+	return (
+		<p className="flex text-3xl font-bold font-mono group-hover:text-orange-500 duration-500 transition-all select-none">
+			cron
+		</p>
+	);
+}
--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/atoms/Button";
 import ViewPicker from "@/components/templates/ViewPicker";
+import Logo from "../atoms/Logo";
 
 interface Props {
 	scrollToCurrentDate: () => void;
@@ -11,9 +12,7 @@ export default function Header({ scrollToCurrentDate, month, year }: Props) {
 	return (
 		<nav className="sticky left-0 top-0 flex h-20 xs:h-14 w-screen items-center border-b bg-background px-2 z-50 gap-4">
 			<div className="flex flex-col xs:flex-row items-center justify-center xs:gap-4">
-				<p className="flex text-3xl font-bold font-mono group-hover:text-orange-500 duration-500 transition-all select-none">
-					cron
-				</p>
+				<Logo />
 				<p className="flex font-semibold h-full items-center justify-center mt-1">
 					{month} {year}
 				</p>
