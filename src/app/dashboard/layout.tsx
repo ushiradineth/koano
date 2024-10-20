1032bc95fd2a37824753b3142b4bbd5a797f1060
Ushira Dineth <ushiradineth@gmail.com>
2024-10-20 20:47:54 +0400

    chore: fmt

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,24 @@
-import Context from "@/components/utils/Context";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-	title: "Cron - Dashboard",
-	description: "By Ushira Dineth",
+  title: "Cron - Dashboard",
+  description: "By Ushira Dineth",
 };
 
 export default function DashboardLayout({
-	children,
-	event,
+  children,
+  event,
 }: {
-	children: React.ReactNode;
-	event: React.ReactNode;
+  children: React.ReactNode;
+  event: React.ReactNode;
 }) {
-	return (
-		<Suspense>
-			<Context>
-				<section>
-					{children}
-					{event}
-				</section>
-			</Context>
-		</Suspense>
-	);
+  return (
+    <Suspense>
+      <section>
+        {children}
+        {event}
+      </section>
+    </Suspense>
+  );
 }
