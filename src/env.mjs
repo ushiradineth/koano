9267853684a2642b1cdf2d4b5584dab949cda30b
Ushira Dineth <ushiradineth@gmail.com>
2024-10-20 23:34:26 +0400

    feat: add env validation

---
+++ src/env.mjs
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    API_URL: z.string().url(),
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string().min(1),
+  },
+
+  client: {},
+
+  runtimeEnv: {
+    API_URL: process.env.API_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+});
