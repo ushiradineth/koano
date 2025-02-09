db205245b75d3ed4ef80f18d4339ed02fd54b3ab
Ushira Dineth <ushiradineth@gmail.com>
2025-02-10 01:10:48 +0400

    fix: root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="no-scrollbar select-none">
